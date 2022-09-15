f4d7d9dd2148b47558fca090eddecfb3fb9b484b
mtsrp <matheus.rocha.pimenta@hotmail.com>
2022-09-15 23:07:25 +0100

    First Commit - Spring Initializr

---
+++ src/main/java/pt/matheusrocha/APIRestJunit/ApiRestJunitApplication.java
@@ -0,0 +1,13 @@
+package pt.matheusrocha.APIRestJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/matheusrocha/APIRestJunit/ApiRestJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.matheusrocha.APIRestJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
